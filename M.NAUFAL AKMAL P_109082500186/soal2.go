0ae3457baac304eec5884348ed180f942cb979fc
naufalakmal <akmalprabowo27@gmail.com>
2026-03-31 19:04:54 +0700

    Add files via upload

---
+++ M.NAUFAL AKMAL P_109082500186/soal2.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func hitungSkor(soal *int, skor *int) {
+	var waktu int
+	*soal = 0
+	*skor = 0
+
+	for i := 0; i < 8; i++ {
+		fmt.Scan(&waktu)
+
+		if waktu <= 300 {
+			*soal = *soal + 1
+			*skor = *skor + waktu
+		}
+	}
+}
+
+func main() {
+	var nama string
+	var pemenang string
+	var soal, skor int
+	var maxSoal = -1
+	var minSkor = 999999
+
+	for {
+		fmt.Scan(&nama)
+
+		if nama == "Selesai" {
+			break
+		}
+
+		hitungSkor(&soal, &skor)
+
+		if soal > maxSoal || (soal == maxSoal && skor < minSkor) {
+			maxSoal = soal
+			minSkor = skor
+			pemenang = nama
+		}
+	}
+
+	fmt.Println(pemenang, maxSoal, minSkor)
+}
